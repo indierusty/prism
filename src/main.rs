7529c56cc3d12569b7cc39326feeab9b8765a00e
indierusty <priyaayadav@gmail.com>
2025-03-04 19:10:36 +0530

    perspective projection of simple cube

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,10 @@ impl Canvas {
         }
     }
 
+    pub fn clear(&mut self) {
+        self.pixels = [WHITE; Self::WIDTH * Self::HEIGHT];
+    }
+
     pub fn draw(&self) {
         for x in 0..Self::WIDTH {
             for y in 0..Self::HEIGHT {
@@ -33,19 +37,44 @@ impl Canvas {
     }
 }
 
+fn cube() -> Vec<Vec3> {
+    let mut vertices = Vec::new();
+    for x in 0..=10 {
+        for y in 0..=10 {
+            for z in 0..=10 {
+                let (x, y, z) = (x - 5, y - 5, z - 5);
+                vertices.push(vec3(x as f32, y as f32, z as f32));
+            }
+        }
+    }
+    vertices
+}
+
 #[macroquad::main("Prism")]
 async fn main() {
-    let mut pixels = Canvas::new();
+    let mut canvas = Canvas::new();
+    let mut angle = 0.;
 
     loop {
         clear_background(BLACK);
-        pixels.draw();
+        canvas.clear();
 
-        for x in (0..100).step_by(2) {
-            for y in (0..100).step_by(2) {
-                pixels.set_pixel(x, y, BLUE);
-            }
+        let hw = (Canvas::WIDTH / 2) as f32;
+        let hh = (Canvas::HEIGHT / 2) as f32;
+
+        let cube = cube()
+            .iter()
+            .map(|v| Mat3::from_rotation_y(angle) * *v)
+            .map(|v| vec2(v.x / (v.z + 10.), v.y / (v.z + 10.)))
+            .map(|v| vec2(v.x * 50. + hw, v.y * 50. + hh))
+            .collect::<Vec<Vec2>>();
+
+        angle += 0.05;
+
+        for v in cube {
+            canvas.set_pixel(v.x as usize, v.y as usize, RED);
         }
+        canvas.draw();
         next_frame().await
     }
 }
